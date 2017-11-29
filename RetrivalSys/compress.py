c16ef5387724d02b7e415de0fb50e67733d9dd2e
Yinghan Shen <Cantoria@users.noreply.github.com>
2017-11-29 11:09:44 +0800

    Update compress.py
    改写了Gamma_decode方法

--- RetrivalSys/compress.py
+++ RetrivalSys/compress.py
@@ -1,3 +1,4 @@
+import struct
 
 # Gamma编码
 def Gamma_encode(num):
@@ -5,18 +6,20 @@ def Gamma_encode(num):
         raise TypeError("num type must be int")
     if num == 0:
         raise ValueError("Gamma encode num = 0")
+    #把原字符转换为二进制编码，去掉第一个bit
     ob_code = bin(num)[2:]
     l_code = ''
     b_code = ''
+    #一元码l_code
     for i in range(len(ob_code) - 1):
         l_code += '1'
     l_code += '0'
     if len(ob_code) > 1:
         b_code = ob_code[1:]
+    #最终的gamma编码
     code_ult = l_code + b_code
     return code_ult
-
-# Gamma解码
+'''
 def Gamma_decode(code):
     if not isinstance(code, bytes):
         raise TypeError("code type must be bytes")
@@ -28,4 +31,32 @@ def Gamma_decode(code):
             break
     num = 2 ** num_len + int(code[num_len + 1:], 2)
     return num
+'''
+#将一个二进制串解码，解码结果以list形式返回
+def Gamma_decode(code):
+    if not isinstance(code, str):
+        raise TypeError("code type must be bytes")
+    num = []
+    flag = 0
+    code_length = len(code)
+    while flag < code_length:
+        num_len = 0
+        for i in range(flag, code_length):
+            if code[i] == '1':
+                num_len += 1
+            else:
+                break
+        start = flag + num_len + 1
+        end = flag + 2 * num_len + 1
+        if(start == end):
+            tmp_num = 0
+        else:
+            tmp_num = 2 ** num_len + int(code[start:end], 2)
+        #print(start)
+        num.append(tmp_num)
+        flag = end
+    return num
+
+#print(Gamma_decode('1100011100001010'))
+
 
