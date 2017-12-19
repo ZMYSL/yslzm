3d3a21277873611563436fd64e2d3f54207cf181
jyzhao <469413628@qq.com>
2017-12-19 17:35:01 +0800

    update compress.py 现在对连续倒排记录压缩及获取偏移量Gamma_encode_all已经实现，下一步实现连续解码

--- RetrivalSys/compress.py
+++ RetrivalSys/compress.py
@@ -35,6 +35,59 @@ def Gamma_encode(num):
 
     return bytes(num_list)
 
+# Gamma编码,对一个list进行编码
+def Gamma_encode_all(numlist):
+    """Gamma编码:
+                Args:
+                    numlist: int型list.
+                Output:
+                    code_list: bytes型数组，不定长，末尾字节是从高位开始编码，低位可能没有使用.
+                    bias_list: int型数组，保留每个IDF，即倒排起始位偏移量
+                """
+    if not isinstance(numlist, list):
+        raise TypeError("numlist type must be list")
+    code_list = []
+    code_ult = ''
+    bias_list = []
+    bias = 0
+    get_df = False
+    for i in range(len(numlist)):
+        # if not isinstance(numlist[i], list):
+        #     raise TypeError("numlist element type must be int")
+        if numlist[i] == 0:
+            raise ValueError("Gamma encode num = 0")
+        if not get_df:
+            get_df = True
+            bias_list.append(bias)
+            df = numlist[i] * 2
+            doc_sum = 0
+        else:
+            doc_sum += 1
+            if doc_sum >= df:
+                get_df = False
+        ob_code = bin(numlist[i])[2:]
+        l_code = ''
+        b_code = ''
+        for i in range(len(ob_code) - 1):
+            l_code += '1'
+        l_code += '0'
+        if len(ob_code) > 1:
+            b_code = ob_code[1:]
+        # 先得到字符串编码，01二进制但是是str类型
+        now_code = l_code + b_code
+        code_ult += now_code
+        bias += len(now_code)
+
+    bit8 = 0
+    # 从高位开始8位存成一个整数，然后用bytes重构
+    while bit8 < len(code_ult):
+        now = bit8
+        end = bit8 + 8 if bit8 + 8 <= len(code_ult) else len(code_ult)
+        code_list.append(int(code_ult[now:end], 2) << (8 - (end - now)))
+        bit8 += 8
+    print(code_ult)
+    return bytes(code_list), bias_list
+
 # Gamma解码
 def Gamma_decode(code):
     """Gamma编码:
@@ -79,6 +132,72 @@ def Gamma_decode(code):
 
     num += 1 << num_len
     return num
+
+# Gamma解码
+def Gamma_decode_all(code, bias):
+    """Gamma解码:从指定偏移位置解出对应的倒排记录
+                Args:
+                    code: bytes型数组.
+                    bias: int型变量
+                Output:
+                    numlist: 解码后的倒排表.
+                """
+    if not isinstance(code, bytes):
+        raise TypeError("code type must be bytes")
+    if not isinstance(bias, int):
+        raise TypeError("bias type must be bytes")
+    num_len = 0
+    code_len = 0
+    get_len = False
+
+    bytes_id = bias // 8
+    bias_id = bias % 8
+
+    get_df = False
+    df = 0
+    for i in range(bytes_id, len(code)):
+        # 先解长度码
+        if i == bytes_id:
+            now_num = code[i] & (1 << (8 - bias_id) - 1)
+        if code[i] == 255 and not get_len:
+            num_len += 8
+        elif not get_len:
+            if i > bytes_id:
+                now_num = code[i]
+            now_len = 0
+            # 从高位找1
+            for j in range(8):
+                if (1 << (7 - j)) <= now_num:
+                    now_num -= (1 << (7 - j))
+                    now_len += 1
+                else:
+                    get_len = True
+                    len_end = j + 1
+                    num = 0
+                    break
+            if get_len:
+                l = 0
+                for j in range(len_end, 8):
+                    if l < now_len:
+                        num += (now_num & (1 << 7 - j))
+                        l += 1
+
+
+            # code_len += 16-now_len-1
+            # num_len += now_len
+            # num = now_num
+        else:
+            # 解剩余的偏移码我偷懒直接移位求和了，实际还需要改进，按解得的长度去解码
+            if code_len<num_len:
+                num = (num << 8)+code[i]
+                code_len += 8
+            else:
+                left_len = num_len + 8 - code_len
+                num = (num << left_len) + (code[i] >> 8 - left_len)
+
+    num += 1 << num_len
+    return num
+
 '''
 bt = Gamma_encode(1025)
 print(bt)
@@ -118,11 +237,18 @@ def index_encode():
     encode_index_file.close()
 
 def index_decode(num):
-    encode_index_file = open("components/encode_index.txt", 'rb')
+    encode_index_file = open("components/Drama/encode_index.txt", 'rb')
     code=encode_index_file.read()
     print(code[num])
 
 
 
-index_encode()
+# index_encode()
 #index_decode(58)
+
+numlist = [2, 2, 2, 2, 2, 1, 2, 2]
+code, bias = Gamma_encode_all(numlist)
+print(code)
+print(bias)
+
+# Gamma_decode_all(code, 15)
