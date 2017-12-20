f87d9fa2523e5df47141944935c8c48b8dffe175
jyzhao <469413628@qq.com>
2017-12-20 23:19:22 +0800

    update make_index.py and compress.py

--- RetrivalSys/compress.py
+++ RetrivalSys/compress.py
@@ -1,39 +1,5 @@
 # -*- coding: utf-8 -*-
 import json
-import struct
-import binascii
-# Gamma编码
-def Gamma_encode(num):
-    """Gamma编码:
-            Args:
-                num: int型数字.
-            Output:
-                num_list: bytes型数组，不定长，末尾字节是从高位开始编码，低位可能没有使用.
-            """
-    if not isinstance(num, int):
-        raise TypeError("num type must be int")
-    if num == 0:
-        raise ValueError("Gamma encode num = 0")
-    ob_code = bin(num)[2:]
-    l_code = ''
-    b_code = ''
-    for i in range(len(ob_code) - 1):
-        l_code += '1'
-    l_code += '0'
-    if len(ob_code) > 1:
-        b_code = ob_code[1:]
-    # 先得到字符串编码，01二进制但是是str类型
-    code_ult = l_code + b_code
-    num_list = []
-    bit8 = 0
-    # 从高位开始8位存成一个整数，然后用bytes重构
-    while bit8 < len(code_ult):
-        now = bit8
-        end = bit8 + 8 if bit8 + 8 <= len(code_ult) else len(code_ult)
-        num_list.append(int(code_ult[now:end], 2))
-        bit8 += 8
-
-    return bytes(num_list)
 
 # Gamma编码,对一个list进行编码
 def Gamma_encode_all(numlist):
@@ -41,7 +7,7 @@ def Gamma_encode_all(numlist):
                 Args:
                     numlist: int型list.
                 Output:
-                    code_list: bytes型数组，不定长，末尾字节是从高位开始编码，低位可能没有使用.
+                    code_list: bytes型数组，不定长，末尾字节是从高位开始编码，低位用0补足8位.
                     bias_list: int型数组，保留每个IDF，即倒排起始位偏移量
                 """
     if not isinstance(numlist, list):
@@ -88,167 +54,130 @@ def Gamma_encode_all(numlist):
     print(code_ult)
     return bytes(code_list), bias_list
 
-# Gamma解码
-def Gamma_decode(code):
-    """Gamma编码:
-                Args:
-                    code: bytes型数组，.
-                Output:
-                    num: 解码后的数字.
-                TODO: 现在只能解标准的一个正确编码，
-                """
-    if not isinstance(code, bytes):
-        raise TypeError("code type must be bytes")
-    num = 0
-    num_len = 0
-    code_len = 0
-    get_len = False
-    for i in range(len(code)):
-        # 先解长度码
-        if code[i] == 255 and not get_len:
-            num_len += 8
-        elif not get_len:
-            now_num = code[i]
-            now_len = 0
-            # 从高位找1
-            for j in range(8):
-                if (1 << (7 - j)) <= now_num:
-                    now_num -= (1 << (7 - j))
-                    now_len += 1
-                else:
-                    break
-            code_len += 16-now_len-1
-            num_len += now_len
-            num = now_num
-            get_len = True
-        else:
-            # 解剩余的偏移码我偷懒直接移位求和了，实际还需要改进，按解得的长度去解码
-            if code_len<num_len:
-                num = (num << 8)+code[i]
-                code_len += 8
-            else:
-                left_len=num_len+8-code_len
-                num = (num << left_len) + (code[i] >> 8 - left_len)
-
-    num += 1 << num_len
-    return num
-
-# Gamma解码
-def Gamma_decode_all(code, bias):
+# Gamma解码,直接从文件中在偏移位置进行解码
+def Gamma_decode_all(bias):
     """Gamma解码:从指定偏移位置解出对应的倒排记录
                 Args:
-                    code: bytes型数组.
                     bias: int型变量
                 Output:
-                    numlist: 解码后的倒排表.
+                    index: 解码后的倒排表,和SPIMI生成的格式一致.
                 """
-    if not isinstance(code, bytes):
-        raise TypeError("code type must be bytes")
     if not isinstance(bias, int):
         raise TypeError("bias type must be bytes")
-    num_len = 0
-    code_len = 0
-    get_len = False
-
     bytes_id = bias // 8
     bias_id = bias % 8
-
-    get_df = False
-    df = 0
-    for i in range(bytes_id, len(code)):
-        # 先解长度码
-        if i == bytes_id:
-            now_num = code[i] & (1 << (8 - bias_id) - 1)
-        if code[i] == 255 and not get_len:
-            num_len += 8
-        elif not get_len:
-            if i > bytes_id:
-                now_num = code[i]
-            now_len = 0
-            # 从高位找1
-            for j in range(8):
-                if (1 << (7 - j)) <= now_num:
-                    now_num -= (1 << (7 - j))
-                    now_len += 1
-                else:
-                    get_len = True
-                    len_end = j + 1
-                    num = 0
-                    break
-            if get_len:
-                l = 0
-                for j in range(len_end, 8):
-                    if l < now_len:
-                        num += (now_num & (1 << 7 - j))
-                        l += 1
+    encode_index_file = open("components/Drama/encode_index.txt", 'rb')
+    block = 8192 * 1024
+    block_sum = bytes_id // block
+    bytes_rest = bytes_id % block
+    for i in range(block_sum):
+        encode_index_file.read(block_sum)
+    encode_index_file.read(bytes_rest)
 
 
-            # code_len += 16-now_len-1
-            # num_len += now_len
-            # num = now_num
-        else:
-            # 解剩余的偏移码我偷懒直接移位求和了，实际还需要改进，按解得的长度去解码
-            if code_len<num_len:
-                num = (num << 8)+code[i]
-                code_len += 8
+    def decode_pos(lastbyte, bitid):
+        get_len = False
+        num_len = 0
+        l = 0
+        num = 0
+        # for i in range(byteid, len(code)):
+        byte_id = 0
+        read = False
+        if lastbyte == None:
+            read = True
+        while True:
+            if read:
+                code = encode_index_file.read(1)[0]
             else:
-                left_len = num_len + 8 - code_len
-                num = (num << left_len) + (code[i] >> 8 - left_len)
+                code = lastbyte
+                read = True
+            if byte_id == 0:
+                now_num = code & ((1 << (8 - bitid)) - 1)
+            else:
+                now_num = code
+            if not get_len and now_num == 255:
+                num_len += 8
+            elif not get_len:
+                # 从高位找1
+                len_end = 0
+                if byte_id == 0:
+                    startbit = bitid
+                else:
+                    startbit = 0
+                for j in range(startbit, 8):
+                    if (1 << (7 - j)) <= now_num:
+                        now_num -= (1 << (7 - j))
+                        num_len += 1
+                    else:
+                        len_end = j + 1
+                        get_len = True
+                        num = 0
+                        l = 0
+                        if l + 8 - len_end < num_len:
+                            num += now_num
+                            l += 8 - len_end
+                        else:
+                            rl = 8 - len_end - num_len
+                            lastbit = len_end + num_len
+                            num += now_num >> rl
+                            num += 1 << num_len
+                            lb = code if lastbit < 8 else None
+                            return num, lastbit % 8, lb
+                        break
+            else:
+                if l + 8 < num_len:
+                    l += 8
+                    num = (num << 8) + code
+                else:
+                    needl = num_len - l
+                    rl = 8 - needl
+                    lastbit = needl
+                    num = (num << needl) + (now_num >> rl)
+                    num += 1 << num_len
+                    lb = code if lastbit < 8 else None
+                    return num, lastbit % 8, lb
+            byte_id += 1
+    lb = None
+    df, nbit_id, lb = decode_pos(lb, bias_id)
+    index = []
+    index.append(df)
+    for i in range(df):
+        tf, nbit_id, lb = decode_pos(lb, nbit_id)
+        docid, nbit_id, lb = decode_pos(lb, nbit_id)
+        index.append([tf, docid])
+    return index
 
-    num += 1 << num_len
-    return num
 
-'''
-bt = Gamma_encode(1025)
-print(bt)
-print(type(bt))
-num = Gamma_decode(b'\xff\xc0\x0f')
-print(num)
-'''
 def index_encode():
-    first_index = 0
-    bia = 0
-    k=0
     index_file = open("components/Drama/Index_SPIMI.txt", 'r', encoding='utf-8')
-    #index_file = open("components/index.txt", 'r', encoding='utf-8')
-    encode_index_file = open("components/encode_index.txt", 'wb')
-    dict_file = open("components/dict.txt", 'w', encoding='utf-8')
-    line = index_file.readline()#调用文件的 readline()方法
-    while line:
-        word_index_dict=json.loads(line)
-        index_list=list(word_index_dict.values())[0]
-        first_index=first_index+bia
-        term_dict = {list(word_index_dict.keys())[0]: first_index}
-        term_str = json.dumps(term_dict)
-        dict_file.write(term_str + '\n')
-        code=Gamma_encode(index_list[0])
-        bia+=len(code)
-        encode_index_file.write(code)
-        for i in range(1,len(index_list)):
-            code1 = Gamma_encode(index_list[i][0])
-            code2 = Gamma_encode(index_list[i][1])
-            bia += len(code1)
-            bia += len(code2)
-            encode_index_file.write(code1)
-            encode_index_file.write(code2)
-        line = index_file.readline()
-
+    encode_index_file = open("components/Drama/encode_index.txt", 'wb')
+    dict_file = open("components/Drama/dict.txt", 'w', encoding='utf-8')
+    numlist = []
+    word_dict = []
+    for line in index_file:
+        word_index_dict = json.loads(line)
+        for key, value in word_index_dict.items():
+            word_dict.append(key)
+            df = value[0]
+            numlist.append(df)
+            for i in range(df):
+                numlist.append(value[i + 1][0])
+                numlist.append(value[i + 1][1])
+    codes, biaslist = Gamma_encode_all(numlist)
+    assert len(word_dict) == len(biaslist)
+    for i in range(len(biaslist)):
+        bias_dict = dict()
+        bias_dict[word_dict[i]] = biaslist[i]
+        dict_file.write(json.dumps(bias_dict) + '\n')
+    encode_index_file.write(codes)
     index_file.close()
     encode_index_file.close()
 
-def index_decode(num):
-    encode_index_file = open("components/Drama/encode_index.txt", 'rb')
-    code=encode_index_file.read()
-    print(code[num])
 
 
 
 # index_encode()
-#index_decode(58)
-
-numlist = [2, 2, 2, 2, 2, 1, 2, 2]
-code, bias = Gamma_encode_all(numlist)
-print(code)
-print(bias)
-
-# Gamma_decode_all(code, 15)
+li = [19, 2, 2, 3, 1, 4, 6, 5, 1, 6, 1, 7, 1, 8, 1, 9, 1, 11, 5, 12, 4, 13, 3, 15, 1, 16, 5, 17, 1, 18, 2, 19, 2, 20, 13, 21, 1, 22, 8]
+Gamma_encode_all(li)
+index_list = Gamma_decode_all(1102)
+print(index_list)
--- RetrivalSys/make_index.py
+++ RetrivalSys/make_index.py
@@ -3,8 +3,6 @@ import json
 import os
 import queue
 
-
-
 # 给优先队列的自定义结构
 class Term(object):
     def __init__(self, word, index_list, id):
@@ -149,7 +147,7 @@ def SPIMI(args, file_name, block):
                 else:
                     first_term.index_list[0] += top_term.index_list[0] - 1
                     first_term.index_list[-1][1] += top_term.index_list[1][1]
-                    for item in top_term.index_list[1:]:
+                    for item in top_term.index_list[2:]:
                         first_term.index_list.append(item)
                 merged_flag[top_term.id] = False
             else:
@@ -166,50 +164,3 @@ def SPIMI(args, file_name, block):
             out_file.write(term_str + '\n')
     out_file.close()
 
-
-
-    # for i in range(len(block_lists)):
-    #     block_file = open("components/" + block_lists[i], 'r', encoding='utf-8')
-    #     new_file = open("components/merged" + str(i), 'w', encoding='utf-8')
-    #     m_line = merged_file.readline()
-    #     print(i)
-    #     while m_line:
-    #         m_term = json.loads(m_line)
-    #         b_line = block_file.readline()
-    #         if not b_line:
-    #             while m_line:
-    #                 new_file.write(m_line)
-    #                 m_line = merged_file.readline()
-    #             break
-    #         b_term = json.loads(b_line)
-    #         for key in b_term.keys():
-    #             b_key = key
-    #         for key in m_term.keys():
-    #             m_key = key
-    #         if m_key == b_key:
-    #             tf = b_term[b_key][0]
-    #             sameid = 0
-    #             # 检查收尾是否docid一致
-    #
-    #             for block_index in b_term[b_key][1:]:
-    #                 m_term[m_key].append(block_index)
-    #             m_str = json.dumps(m_term)
-    #             new_file.write(m_str + '\n')
-    #         elif m_key > b_key:
-    #             new_file.write(b_line)
-    #             new_file.write(m_line)
-    #         else:
-    #             new_file.write(m_line)
-    #             new_file.write(b_line)
-    #         m_line = merged_file.readline()
-    #     b_line = block_file.readline()
-    #     while b_line:
-    #         new_file.write(b_line)
-    #         b_line = block_file.readline()
-    #     merged_file.close()
-    #     new_file.close()
-    #     block_file.close()
-    #     merged_file = open("components/merged" + str(i), 'r', encoding='utf-8')
-
-
-# SPIMI("../Data/Drama/shakespeare-merchant_nolabel", 1000)
